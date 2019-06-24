acf36e9fac690126501d22b61698c599c47e60fe
Luís Eduardo <le.barros20166@hotmail.com>
2019-06-23 21:46:32 -0400

    Adicionando projeto concluído no GitHub

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,22 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <cloud_firestore/CloudFirestorePlugin.h>
+#import <firebase_auth/FirebaseAuthPlugin.h>
+#import <firebase_core/FirebaseCorePlugin.h>
+#import <path_provider/PathProviderPlugin.h>
+#import <url_launcher/UrlLauncherPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTCloudFirestorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTCloudFirestorePlugin"]];
+  [FLTFirebaseAuthPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseAuthPlugin"]];
+  [FLTFirebaseCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseCorePlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [FLTUrlLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTUrlLauncherPlugin"]];
+}
+
+@end
